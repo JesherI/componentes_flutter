610eb1997d66eb4556daf6e48daf2529ffa6dfa8
Jesher <jesherjesher1@gmail.com>
2024-02-20 11:57:53 -0600

    Add GoogleFonts and ThextTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,9 +1,12 @@
 // Definición de los temas de estilo de la app
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   //Constante de color primario
   static const primaryColor = Color.fromARGB(255, 255, 0, 0);
+  //Cosnatnate de color secundario
+  static const secondaryColor = Color.fromARGB(155, 0, 0, 0);
   //constante de color de fondo
   static const backColor = Color.fromARGB(218, 253, 213, 193);
   //Constante de tema
@@ -11,6 +14,30 @@ class AppTheme {
     scaffoldBackgroundColor: backColor,
     appBarTheme: const AppBarTheme(
       color: primaryColor,
+    ),
+    textTheme: TextTheme(
+      //Titulos muy grandes
+      headlineLarge: GoogleFonts.outfit(
+        color: primaryColor,
+        fontSize: 26.5,
+        fontWeight: FontWeight.w500,
+        fontStyle: FontStyle.italic,
+        // decoration: TextDecoration.underline,
+        // decorationColor: primaryColor,
+        // decorationStyle: TextDecorationStyle.wavy,
+        // decorationThickness: 3.0,
+      ),
+      //estilo para text muy pequeño
+      bodySmall: GoogleFonts.montserrat(
+        color: secondaryColor,
+        fontSize: 16.0,
+        fontWeight: FontWeight.w500,
+        fontStyle: FontStyle.italic,
+        // decoration: TextDecoration.underline,
+        // decorationColor: primaryColor,
+        // decorationStyle: TextDecorationStyle.wavy,
+        // decorationThickness: 3.0,
+      ),
     ),
   );
 }
