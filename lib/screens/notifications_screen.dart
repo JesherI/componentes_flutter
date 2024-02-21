a7ab280257685713821637efbeecd00fb42023b1
Jesher <jesherjesher1@gmail.com>
2024-02-21 10:40:14 -0600

    Adding inputs, infinite_scroll, notifications and data screens

---
+++ lib/screens/notifications_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotificacionsScreen extends StatelessWidget {
+  const NotificacionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Ventana de notificaciones'),
+    );
+  }
+}
